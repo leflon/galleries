bb9f1f87b243be722b5e30a6bec313cc808d7ac8
Paul Leflon <leflon.paul@gmail.com>
2025-04-18 16:45:23 +0200

    feat(environment): update environment configuration and add example file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
 import {getAuth, provideAuth} from '@angular/fire/auth';
 import {getFirestore, provideFirestore} from '@angular/fire/firestore';
 import {provideRouter} from '@angular/router';
-import {environment} from '../environment';
+import {environment} from '../environments/environment';
 import {routes} from './app.routes';
 import {getFunctions, provideFunctions} from '@angular/fire/functions';
 import {getStorage, provideStorage} from '@angular/fire/storage';
---
+++ src/environments/environment.example.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  apiKey: "YOUR-API-KEY",
+  authDomain: "YOUR-AUTH-DOMAIN",
+  projectId: "YOUR-PROJECT-ID",
+  storageBucket: "YOUR-STORAGE-BUCKET",
+  messagingSenderId: "YOUR-MESSAGING-SENDER-ID",
+  appId: "YOUR-APP_ID",
+  measurementId: "YOUR-MEASUREMENT-ID",
+  functionsDomain: "YOUR-FUNCTIONS-DOMAIN"
+};
